abd1c5258397a4b513feb2b8f59b24109df235d3
femmytedrey <femmytedrey@gmail.com>
2023-10-04 01:49:32 +0100

    Hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import ClassCounter from './component/ClassCounter';
 import HookCounter from './component/HookCounter';
+import HookCounter4 from './component/HookCounter4';
 import HookCounterThree from './component/HookCounterThree';
 import HookCounterTwo from './component/HookCounterTwo';
 
 function App() {
   return (
     <div className="App">
-      <HookCounterThree />
+      <HookCounter4 />
+      {/* <HookCounterThree /> */}
       {/* <HookCounterTwo /> */}
       {/* <HookCounter /> */}
       {/* <ClassCounter /> */}
