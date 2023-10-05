2b756e30c638389212c54ed1575bb7134e06688c
femmytedrey <femmytedrey@gmail.com>
2023-10-05 23:28:51 +0100

    UseEffect

--- src/component/ClassCounterOne.js
+++ src/component/ClassCounterOne.js
@@ -5,7 +5,8 @@ export class ClassCounterOne extends Component {
       super(props)
     
       this.state = {
-         count: 0
+         count: 0,
+         name: ''
       }
     }
     componentDidMount(){
@@ -13,11 +14,15 @@ export class ClassCounterOne extends Component {
     }
 
     componentDidUpdate(prevProps, prevState){
-        document.title = `Clicked ${this.state.count} times`
+        if(prevState.count !== this.state.count){
+            console.log('Updating document title')
+            document.title = `Clicked ${this.state.count} times`
+        }
     }
   render() {
     return (
       <div>
+        <input type="text" value = {this.state.name} onChange={e => {this.setState({name: e.target.value})}}/>
         <button onClick={() => this.setState({count: this.state.count + 1})}>Clicked {this.state.count} times</button>
       </div>
     )
