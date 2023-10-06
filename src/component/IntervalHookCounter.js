a1b1ba1f0ac01bf92df5c31a911bf06d9be62495
femmytedrey <femmytedrey@gmail.com>
2023-10-06 02:23:46 +0100

    useEffect

--- src/component/IntervalHookCounter.js
+++ src/component/IntervalHookCounter.js
@@ -1,16 +1,21 @@
 import React, {useState, useEffect} from 'react'
 
-function IntervalHookCounter() {
+function IntervalHookCounter({someProp}) {
     const [count, setCount] = useState(0)
     const tick = () => {
-        setCount(count + 1)
+        setCount(prevCount => prevCount + 1)
     }
+    
     useEffect (() => {
+        function doSomething(){
+            console.log(someProp)
+        }
+        doSomething()
         const interval = setInterval(tick, 1000)
         return () => {
             clearInterval(interval)
         }
-    }, [count])
+    }, [someProp])
 
   return (
     <div>
