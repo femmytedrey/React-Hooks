bef3b98aaa81b9f04d459815052a78d53ef7b0db
femmytedrey <femmytedrey@gmail.com>
2023-10-10 23:59:03 +0100

    useReducer

--- src/component/ReducerCounterOne.js
+++ src/component/ReducerCounterOne.js
@@ -19,7 +19,7 @@ function ReducerCounterOne() {
   return (
     <div>
         <p>Count - {count}</p>
-        <button onClick = {() => dispatch('increment')}>Increment</button> 
+        <button onClick = {() => dispatch('increment')}>Increment</button>
         <button onClick={() => dispatch('decrement')}>Decrement</button>
         <button onClick={() => dispatch('reset')}>Reset</button>
     </div>
