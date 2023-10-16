1c8ccd110b6662190ade28f80dbfd5b9d3d61f67
femmytedrey <femmytedrey@gmail.com>
2023-10-16 21:47:49 +0100

    exploring useReducer

---
+++ src/component/DataFetchingTwo.js
@@ -0,0 +1,20 @@
+import React, {useReducer, useEffect} from 'react'
+import axios from 'axios'
+
+const initialState = {
+    loading: true,
+    error: '',
+    post: {}
+}
+
+const reducer = (state, action) => {
+    switch(action.type)
+}
+
+function DataFetchingTwo() {
+  return (
+    <div>DataFetchingTwo</div>
+  )
+}
+
+export default DataFetchingTwo
